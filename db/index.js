8df16796e727277e090476d4194c5a48714f0d7b
srabhiram <sriramoji.abhi@gmail.com>
2024-07-18 07:20:53 +0530

    fix

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(`${process.env.MONGODB_URI}/secureAuth`);
     console.log("Database connected");
   } catch (e) {
     console.log("MongoDB connection error", e);
