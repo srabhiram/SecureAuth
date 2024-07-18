e87dc1a05dc9aa6197b8f605ce6d4aba84bdf451
srabhiram <sriramoji.abhi@gmail.com>
2024-07-18 07:05:36 +0530

    fixed some issues related to deployement

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ connectDB();
 connectCloudinary();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
